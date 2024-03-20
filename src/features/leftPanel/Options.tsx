4f2d1f5813de95352cacccf2daa15757f55c800d
Sandro <vipsandro11@gmail.com>
2024-03-20 19:43:04 +0400

    feat: create Options.tsx

---
+++ src/features/leftPanel/Options.tsx
@@ -0,0 +1,13 @@
+import HiddenOptions from "./options/HiddenOptions";
+import TopOptions from "./options/TopOptions";
+
+const Options = () => {
+  return (
+    <div>
+      <TopOptions />
+      <HiddenOptions />
+    </div>
+  );
+};
+
+export default Options;
