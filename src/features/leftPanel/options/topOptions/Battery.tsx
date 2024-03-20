b7464b069383b42510d57a4161cf26f2a99bc314
Sandro <vipsandro11@gmail.com>
2024-03-20 20:24:53 +0400

    update Battery.tsx

--- src/features/leftPanel/options/topOptions/Battery.tsx
+++ src/features/leftPanel/options/topOptions/Battery.tsx
@@ -1,9 +1,15 @@
-
+import { BsBatteryCharging } from "react-icons/bs";
 
 const Battery = () => {
   return (
-    <div>Battery</div>
-  )
-}
+    <div className="bg-white border rounded-md flex items-center justify-between px-2 text-headerSize">
+      <input type="text" className="py-2 outline-none" defaultValue={"50"} />
+      <div className="flex items-center gap-1 font-bold">
+        <span>Battery</span>
+        <BsBatteryCharging size={15} />
+      </div>
+    </div>
+  );
+};
 
-export default Battery
+export default Battery;
