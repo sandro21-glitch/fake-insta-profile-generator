5f4cf1d971fc092d4bcff4b46425b17381e85cc1
Sandro <vipsandro11@gmail.com>
2024-03-20 20:24:55 +0400

    update Time.tsx

--- src/features/leftPanel/options/topOptions/Time.tsx
+++ src/features/leftPanel/options/topOptions/Time.tsx
@@ -1,9 +1,12 @@
-
+import { IoTimeOutline } from "react-icons/io5";
 
 const Time = () => {
   return (
-    <div>Time</div>
-  )
-}
+    <div className="bg-white border rounded-md flex items-center justify-between px-2 text-headerSize mb-2">
+      <input type="text" className="py-2 outline-none" defaultValue={'10:04 AM'} />
+      <IoTimeOutline size={15} />
+    </div>
+  );
+};
 
-export default Time
+export default Time;
