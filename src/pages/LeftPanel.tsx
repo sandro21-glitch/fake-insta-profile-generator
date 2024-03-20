988b35f76fc4e5e05e18a607ed76f832fd8a6e6e
Sandro <vipsandro11@gmail.com>
2024-03-20 19:43:37 +0400

    update LeftPanel.tsx

--- src/pages/LeftPanel.tsx
+++ src/pages/LeftPanel.tsx
@@ -1,9 +1,11 @@
-
+import Options from "../features/leftPanel/Options";
 
 const LeftPanel = () => {
   return (
-    <div>LeftPanel</div>
-  )
-}
+    <article className="w-1/4 bg-red-900 p-3">
+      <Options />
+    </article>
+  );
+};
 
-export default LeftPanel
+export default LeftPanel;
