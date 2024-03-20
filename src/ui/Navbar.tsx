2f224dc78a64bfa130f594cd19fd73e7ecbbd86a
Sandro <vipsandro11@gmail.com>
2024-03-20 19:43:43 +0400

    update Navbar.tsx

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,9 +1,11 @@
-
-
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="mb-14">
+      <h6 className="text-white inline-block bg-cyan-500 p-1">
+        Fake Instagram Profile Generator
+      </h6>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
