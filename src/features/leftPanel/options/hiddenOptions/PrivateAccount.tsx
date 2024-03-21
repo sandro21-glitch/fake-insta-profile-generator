53c1c61f88ede904e56950133b9e6f08c5b8856e
Sandro <vipsandro11@gmail.com>
2024-03-21 16:36:46 +0400

    update PrivateAccount.tsx

--- src/features/leftPanel/options/hiddenOptions/PrivateAccount.tsx
+++ src/features/leftPanel/options/hiddenOptions/PrivateAccount.tsx
@@ -1,5 +1,11 @@
+import Switch from "../../../../ui/Switch";
+
 const PrivateAccount = () => {
-  return <div>PrivateAccount</div>;
+  return (
+    <div className="mb-3">
+      <Switch optionName="Private Account" />
+    </div>
+  );
 };
 
 export default PrivateAccount;
