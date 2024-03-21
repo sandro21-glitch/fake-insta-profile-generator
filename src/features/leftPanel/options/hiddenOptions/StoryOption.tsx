761871afa8794939fe714407a5b3320d0bf9a9fe
Sandro <vipsandro11@gmail.com>
2024-03-21 16:37:03 +0400

    update StoryOption.tsx

--- src/features/leftPanel/options/hiddenOptions/StoryOption.tsx
+++ src/features/leftPanel/options/hiddenOptions/StoryOption.tsx
@@ -1,9 +1,32 @@
-
+import { useState } from "react";
 
 const StoryOption = () => {
-  return (
-    <div>StoryOption</div>
-  )
-}
+  const [storyStatus, setStoryStatus] = useState("unseen");
 
-export default StoryOption
+  return (
+    <div className="mb-3 flex items-center text-headerSize">
+      <label htmlFor="seen" className="w-1/2">
+        <input
+          type="radio"
+          id="seen"
+          checked={storyStatus === "seen"}
+          onChange={() => setStoryStatus('seen')}
+          className="mr-2"
+        />
+        seen
+      </label>
+      <label htmlFor="unseen" className="w-1/2">
+        <input
+          type="radio"
+          id="unseen"
+          checked={storyStatus === "unseen"}
+          onChange={() => setStoryStatus('unseen')}
+          className="mr-2"
+        />
+        unseen
+      </label>
+    </div>
+  );
+};
+
+export default StoryOption;
