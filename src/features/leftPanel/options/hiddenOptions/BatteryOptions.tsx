ad147a47eecd0c9f1f31121435f6729487ff829f
Sandro <vipsandro11@gmail.com>
2024-03-21 16:36:37 +0400

    update BatteryOptions.tsx

--- src/features/leftPanel/options/hiddenOptions/BatteryOptions.tsx
+++ src/features/leftPanel/options/hiddenOptions/BatteryOptions.tsx
@@ -1,9 +1,14 @@
-
-
 const BatteryOptions = () => {
   return (
-    <div>BatteryOptions</div>
-  )
-}
+    <div className="mb-3">
+      <p className="text-headerSize font-bold mb-1">Battery Options</p>
+      <select className="w-full border outline-none p-1 text-headerSize">
+        <option>Horizontal</option>
+        <option>Vertical-1</option>
+        <option>Vertical-2</option>
+      </select>
+    </div>
+  );
+};
 
-export default BatteryOptions
+export default BatteryOptions;
