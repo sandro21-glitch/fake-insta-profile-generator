824c798e9152ada4cce3054e51330a030d6b2ee7
Sandro <vipsandro11@gmail.com>
2024-03-21 16:59:56 +0400

    feat: create PreviewPart.tsx and LiveBoxModal.tsx

---
+++ src/features/rightPanel/PreviewPart.tsx
@@ -0,0 +1,11 @@
+import LiveBoxModal from "./modals/LiveBoxModal";
+
+const PreviewPart = () => {
+  return (
+    <section className="border min-h-[512px] relative bg-red-900 rounded-md">
+      <LiveBoxModal />
+    </section>
+  );
+};
+
+export default PreviewPart;
---
+++ src/features/rightPanel/modals/LiveBoxModal.tsx
@@ -0,0 +1,9 @@
+const LiveBoxModal = () => {
+  return (
+    <div className="absolute left-5 top-5 shadow-lg p-2 rounded-sm bg-white select-none text-headerSize font-semibold">
+      Live preview
+    </div>
+  );
+};
+
+export default LiveBoxModal;
