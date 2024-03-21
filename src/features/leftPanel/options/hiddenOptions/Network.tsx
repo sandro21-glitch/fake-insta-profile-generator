f99a507b088126f7b895002cabee3c6392e04a18
Sandro <vipsandro11@gmail.com>
2024-03-21 16:36:43 +0400

    update Network.tsx

--- src/features/leftPanel/options/hiddenOptions/Network.tsx
+++ src/features/leftPanel/options/hiddenOptions/Network.tsx
@@ -1,5 +1,16 @@
 const Network = () => {
-  return <div>Network</div>;
+  return (
+    <div className="mb-3">
+      <p className="text-headerSize font-bold mb-1">Network</p>
+      <select className="w-full border outline-none p-1 text-headerSize">
+        <option>5G</option>
+        <option>4G</option>
+        <option>3G</option>
+        <option>2G</option>
+        <option>Wifi</option>
+      </select>
+    </div>
+  );
 };
 
 export default Network;
