d967e9a777415a7d82292e745cc282ff4de0140d
Sandro <vipsandro11@gmail.com>
2024-03-22 19:03:08 +0400

    update LiveBoxModal.tsx

--- src/features/rightPanel/modals/LiveBoxModal.tsx
+++ src/features/rightPanel/modals/LiveBoxModal.tsx
@@ -1,6 +1,6 @@
 const LiveBoxModal = () => {
   return (
-    <div className="absolute left-5 top-5 shadow-lg p-2 rounded-sm bg-white select-none text-headerSize font-semibold">
+    <div className="mb-10 shadow-lg p-2 inline-block rounded-sm bg-white select-none text-headerSize font-semibold">
       Live preview
     </div>
   );
