b91aa55d1fc53bdc846af06708bb1cb7d94b7f5f
Sandro <vipsandro11@gmail.com>
2024-03-22 23:50:54 +0400

    feat: create VerifiedBadge.tsx

---
+++ src/features/rightPanel/modalContent/header/headerBottom/VerifiedBadge.tsx
@@ -0,0 +1,13 @@
+const VerifiedBadge = () => {
+  return (
+    <div>
+      <img
+        src="https://img.icons8.com/?size=256&id=2sZ0sdlG9kWP&format=png"
+        alt="verified badge"
+        className="w-[17px] h-[17px] ml-1"
+      />
+    </div>
+  );
+};
+
+export default VerifiedBadge;
