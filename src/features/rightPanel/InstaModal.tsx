19669b1bb46f276396a0d5fdd45228006cd7cf39
Sandro <vipsandro11@gmail.com>
2024-03-22 19:02:20 +0400

    feat: create InstaModal.tsx

---
+++ src/features/rightPanel/InstaModal.tsx
@@ -0,0 +1,11 @@
+import ModalDialog from "./modalContent/ModalDialog";
+
+const InstaModal = () => {
+  return (
+    <div className="w-[360px] max-w-[1000px] mx-auto ">
+      <ModalDialog />
+    </div>
+  );
+};
+
+export default InstaModal;
