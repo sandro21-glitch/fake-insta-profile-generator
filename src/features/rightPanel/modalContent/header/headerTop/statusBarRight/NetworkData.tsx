15e1a8a6722a19e7441b4c9aab912dbf3262dacf
Sandro <vipsandro11@gmail.com>
2024-03-22 19:02:53 +0400

    feat: create NetworkData.tsx

---
+++ src/features/rightPanel/modalContent/header/headerTop/statusBarRight/NetworkData.tsx
@@ -0,0 +1,8 @@
+// import { CiWifiOn } from "react-icons/ci"; wifi for android
+// import { CiWifiOn } from "react-icons/ci"; wifi for iphone
+
+const NetworkData = () => {
+  return <div className="font-semibold mr-[2px]">5G</div>;
+};
+
+export default NetworkData;
