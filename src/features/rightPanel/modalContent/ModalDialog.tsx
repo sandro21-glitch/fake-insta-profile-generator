0df2b660a02023fa85919f6a8606521a4710b301
Sandro <vipsandro11@gmail.com>
2024-03-22 19:02:22 +0400

    feat: create ModalDialog.tsx

---
+++ src/features/rightPanel/modalContent/ModalDialog.tsx
@@ -0,0 +1,15 @@
+import Footer from "./footer/Footer";
+import Header from "./header/Header";
+import MainBody from "./mainBody/MainBody";
+
+const ModalDialog = () => {
+  return (
+    <div className="bg-white w-full">
+      <Header />
+      <MainBody />
+      <Footer />
+    </div>
+  );
+};
+
+export default ModalDialog;
