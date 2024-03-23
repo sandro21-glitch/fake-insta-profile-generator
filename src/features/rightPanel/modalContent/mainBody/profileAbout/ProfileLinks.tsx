ac1609ad05b85d1c35c025356fb21ebae40f69f0
Sandro <vipsandro11@gmail.com>
2024-03-23 23:14:23 +0400

    update ProfileLinks.tsx

--- src/features/rightPanel/modalContent/mainBody/profileAbout/ProfileLinks.tsx
+++ src/features/rightPanel/modalContent/mainBody/profileAbout/ProfileLinks.tsx
@@ -1,5 +1,68 @@
+import { useRef, useState } from "react";
+import useOutsideClickAndFocus from "../../../../../hooks/useOutsideClickAndFocus";
+import ButtonSection from "./profileLinks/ButtonSection";
+import ProfileLinkDisplay from "./profileLinks/ProfileLinkDisplay";
 const ProfileLinks = () => {
-  return <div>ProfileLinks</div>;
+  const [link, setLink] = useState<string>("");
+  const [linkCount, setLinkCount] = useState("");
+  const [isBtnClicked, setIsBtnClicked] = useState(false);
+  const [isOtherClicked, setIsOtherClicked] = useState(false);
+  const [isInputClicked, setIsInputClicked] = useState(false);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useOutsideClickAndFocus({
+    containerRef,
+    inputRef,
+    isInputClicked,
+    setIsInputClicked,
+  });
+
+  const handleClickFb = () => {
+    if (link !== "") {
+      setLinkCount("1");
+      setIsBtnClicked(true);
+    } else {
+      setLink("Facebook profile");
+      setIsBtnClicked(true);
+    }
+  };
+  const handleClickOther = () => {
+    if (link !== "") {
+      setLinkCount("1");
+      setIsOtherClicked(true);
+    } else {
+      setLink("Enter link text");
+      setIsOtherClicked(true);
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLink(e.target.value);
+  };
+
+  return (
+    <div className="mt-[8px]">
+      {(isBtnClicked || isOtherClicked) && (
+        <ProfileLinkDisplay
+          link={link}
+          linkCount={linkCount}
+          containerRef={containerRef}
+          isInputClicked={isInputClicked}
+          setIsInputClicked={setIsInputClicked}
+          inputRef={inputRef}
+          handleInputChange={handleInputChange}
+        />
+      )}
+      <ButtonSection
+        isBtnClicked={isBtnClicked}
+        isOtherClicked={isOtherClicked}
+        handleClickFb={handleClickFb}
+        handleClickOther={handleClickOther}
+      />
+    </div>
+  );
 };
 
 export default ProfileLinks;
