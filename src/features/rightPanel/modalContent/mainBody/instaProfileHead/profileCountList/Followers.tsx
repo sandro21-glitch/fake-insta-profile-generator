f084dedf4e3d035439eb877f9731995dd498137d
Sandro <vipsandro11@gmail.com>
2024-03-23 15:15:22 +0400

    feat: create Followers.tsx

---
+++ src/features/rightPanel/modalContent/mainBody/instaProfileHead/profileCountList/Followers.tsx
@@ -0,0 +1,43 @@
+import { useRef, useState } from "react";
+import useOutsideClickAndFocus from "../../../../../../hooks/useOutsideClickAndFocus";
+import InputField from "../../../../../../ui/InputField";
+
+const Followers = () => {
+  const [followers, setFollowers] = useState<string>("150K");
+  const [isInputClicked, setIsInputClicked] = useState(false);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useOutsideClickAndFocus({
+    containerRef,
+    inputRef,
+    isInputClicked,
+    setIsInputClicked,
+  });
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFollowers(e.target.value);
+  };
+
+  return (
+    <div className="w-1/3 text-center" ref={containerRef}>
+      <p
+        onClick={() => setIsInputClicked(true)}
+        className={`mb-1 ${isInputClicked ? "hidden" : "block"}`}
+      >
+        {followers}
+      </p>
+      {isInputClicked && (
+        <InputField
+          onChange={handleInputChange}
+          value={followers}
+          inputRef={inputRef}
+          className="w-full text-center"
+        />
+      )}
+      <p>Followers</p>
+    </div>
+  );
+};
+
+export default Followers;
