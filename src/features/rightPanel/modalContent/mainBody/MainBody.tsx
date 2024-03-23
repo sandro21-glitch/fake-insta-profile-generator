d4d51234e73e9c3ba45af3d460a448bb80f58555
Sandro <vipsandro11@gmail.com>
2024-03-23 15:15:04 +0400

    update MainBody.tsx

--- src/features/rightPanel/modalContent/mainBody/MainBody.tsx
+++ src/features/rightPanel/modalContent/mainBody/MainBody.tsx
@@ -1,8 +1,11 @@
+import ProfileHead from "./instaProfileHead/ProfileHead";
 
 const MainBody = () => {
   return (
-    <div>MainBody</div>
-  )
-}
+    <section className="px-4 py-2">
+      <ProfileHead />
+    </section>
+  );
+};
 
-export default MainBody
+export default MainBody;
