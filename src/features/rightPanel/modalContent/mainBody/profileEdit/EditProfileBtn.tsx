5c71b24aa32d21ea947c72a78f8b2e52e9c67705
Sandro <vipsandro11@gmail.com>
2024-03-24 19:30:49 +0400

    feat: create EditProfileBtn.tsx

---
+++ src/features/rightPanel/modalContent/mainBody/profileEdit/EditProfileBtn.tsx
@@ -0,0 +1,9 @@
+const EditProfileBtn = () => {
+  return (
+    <div className="px-[10px] py-[5px] h-[30px] leading-[22px] bg-[#ebebeb] rounded-md font-semibold text-center text-modalHeaderSize flex-1">
+      Edit Profile
+    </div>
+  );
+};
+
+export default EditProfileBtn;
