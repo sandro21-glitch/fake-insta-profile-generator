4459fd2c4b1a99ba95190546972909002f5f6cdb
Sandro <vipsandro11@gmail.com>
2024-03-24 22:18:17 +0400

    feat: create Highlights.tsx

---
+++ src/features/rightPanel/modalContent/mainBody/instaHighlights/Highlights.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import ShowHighlightsBtn from "./ShowHighlightsBtn";
+import MainHighlights from "./mainHighlights/MainHighlights";
+
+const Highlights = () => {
+  const [showHighlights, setShowHighlights] = useState<boolean>(false);
+  return (
+    <div>
+      <ShowHighlightsBtn setShowHighlights={setShowHighlights} showHighlights={showHighlights} />
+      {showHighlights && <MainHighlights />}
+    </div>
+  );
+};
+
+export default Highlights;
