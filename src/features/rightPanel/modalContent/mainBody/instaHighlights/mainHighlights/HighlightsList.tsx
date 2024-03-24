f6f30f8c4a0da1e69692d566aba969df3f0407eb
Sandro <vipsandro11@gmail.com>
2024-03-24 22:53:19 +0400

    update HighlightsList.tsx

--- src/features/rightPanel/modalContent/mainBody/instaHighlights/mainHighlights/HighlightsList.tsx
+++ src/features/rightPanel/modalContent/mainBody/instaHighlights/mainHighlights/HighlightsList.tsx
@@ -1,8 +1,17 @@
+import AddNewHighlight from "./AddNewHighlight";
+import TempListItem from "./TempListItem";
+
+const tempList = [{ id: 1 }, { id: 2 }, { id: 3 }];
 
 const HighlightsList = () => {
   return (
-    <div>HighlightsList</div>
-  )
-}
+    <ul className="flex justify-between">
+      <AddNewHighlight />
+      {tempList.map((list) => {
+        return <TempListItem key={list.id} />;
+      })}
+    </ul>
+  );
+};
 
-export default HighlightsList
+export default HighlightsList;
