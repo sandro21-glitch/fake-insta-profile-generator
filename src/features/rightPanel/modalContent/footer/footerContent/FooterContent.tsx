e7661147e5587d6fa624c5149af0115965622931
Sandro <vipsandro11@gmail.com>
2024-03-24 19:30:38 +0400

    feat: create FooterContent.tsx

---
+++ src/features/rightPanel/modalContent/footer/footerContent/FooterContent.tsx
@@ -0,0 +1,11 @@
+import FooterList from "./FooterList";
+
+const FooterContent = () => {
+  return (
+    <ul className="grid grid-cols-5 place-items-center w-full">
+      <FooterList />
+    </ul>
+  );
+};
+
+export default FooterContent;
