ece77492f8c2014d1bcf5e0b4ee15c122923e27a
Sandro <vipsandro11@gmail.com>
2024-03-24 17:48:18 +0400

    feat: create LinkCount.tsx

---
+++ src/features/rightPanel/modalContent/mainBody/profileAbout/profileLinks/LinkCount.tsx
@@ -0,0 +1,42 @@
+import { useRef, useState } from "react";
+import InputField from "../../../../../../ui/InputField";
+import useOutsideClickAndFocus from "../../../../../../hooks/useOutsideClickAndFocus";
+
+type LinkCountTypes = {
+  linkCount: string;
+  setLinkCount: (link: string) => void;
+};
+const LinkCount = ({ linkCount, setLinkCount }: LinkCountTypes) => {
+  const [isInputClicked, setIsInputClicked] = useState(false);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useOutsideClickAndFocus({
+    containerRef,
+    inputRef,
+    isInputClicked,
+    setIsInputClicked,
+  });
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLinkCount(e.target.value);
+  };
+
+  return (
+    <div ref={containerRef}>
+      {isInputClicked ? (
+        <InputField
+          value={linkCount}
+          inputRef={inputRef}
+          onChange={handleInputChange}
+        />
+      ) : (
+        <p onClick={() => setIsInputClicked(true)} className="ml-1 text-fbLink">
+          and {linkCount} other link
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default LinkCount;
