2e0e36cfab1dbee3dfa0c1dea3024061c6a7ce66
Sandro <vipsandro11@gmail.com>
2024-03-24 22:53:26 +0400

    feat: create TempListItem.tsx

---
+++ src/features/rightPanel/modalContent/mainBody/instaHighlights/mainHighlights/TempListItem.tsx
@@ -0,0 +1,9 @@
+const TempListItem = () => {
+  return (
+    <li className="min-w-[80px] w-[80px] flex flex-col items-center">
+      <div className="w-[63px] h-[63px] bg-bg2Color rounded-full mt-[3px] cursor-pointer text-modalHeaderSize"></div>
+    </li>
+  );
+};
+
+export default TempListItem;
