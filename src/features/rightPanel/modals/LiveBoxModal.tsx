034d50b757136ec1c9f397a8f0af25c8f6ac2608
Sandro <vipsandro11@gmail.com>
2024-03-24 23:31:16 +0400

    chore: delete LiveBoxModal.tsx

--- src/features/rightPanel/modals/LiveBoxModal.tsx
+++
@@ -1,9 +0,0 @@
-const LiveBoxModal = () => {
-  return (
-    <div className="mb-10 shadow-lg p-2 inline-block rounded-sm bg-white select-none text-headerSize font-semibold">
-      Live preview
-    </div>
-  );
-};
-
-export default LiveBoxModal;
