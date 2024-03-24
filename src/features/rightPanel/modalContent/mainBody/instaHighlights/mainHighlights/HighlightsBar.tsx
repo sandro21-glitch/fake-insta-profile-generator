d132715775a47ca3aa3228684f518b19176afe8a
Sandro <vipsandro11@gmail.com>
2024-03-24 22:18:10 +0400

    feat: create HighlightsBar.tsx

---
+++ src/features/rightPanel/modalContent/mainBody/instaHighlights/mainHighlights/HighlightsBar.tsx
@@ -0,0 +1,9 @@
+const HighlightsBar = () => {
+  return (
+    <div className="mb-[10px] text-modalHeaderSize">
+      Keep your favorite stories on your profile
+    </div>
+  );
+};
+
+export default HighlightsBar;
