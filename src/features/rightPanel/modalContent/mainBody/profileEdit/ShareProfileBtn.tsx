ef55264d07b37da8fd92d2bdac697b57feebc728
Sandro <vipsandro11@gmail.com>
2024-03-24 19:30:56 +0400

    feat: create ShareProfileBtn.tsx

---
+++ src/features/rightPanel/modalContent/mainBody/profileEdit/ShareProfileBtn.tsx
@@ -0,0 +1,9 @@
+const ShareProfileBtn = () => {
+  return (
+        <div className="px-[10px] py-[5px] h-[30px] leading-[22px] bg-[#ebebeb] rounded-md font-semibold text-modalHeaderSize text-center flex-1">
+      Edit Profile
+    </div>
+  );
+};
+
+export default ShareProfileBtn;
