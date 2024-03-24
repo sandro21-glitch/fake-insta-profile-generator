e03cb21391020638eb4ec82932a07724b347120b
Sandro <vipsandro11@gmail.com>
2024-03-24 22:18:04 +0400

    feat: create MainHighlights.tsx

---
+++ src/features/rightPanel/modalContent/mainBody/instaHighlights/mainHighlights/MainHighlights.tsx
@@ -0,0 +1,13 @@
+import HighlightsBar from "./HighlightsBar";
+import HighlightsList from "./HighlightsList";
+
+const MainHighlights = () => {
+  return (
+    <div>
+      <HighlightsBar />
+      <HighlightsList />
+    </div>
+  );
+};
+
+export default MainHighlights;
