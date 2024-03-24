5e3a9bb12a51393984b38d6fabfd4ab66ad1e191
Sandro <vipsandro11@gmail.com>
2024-03-24 23:39:54 +0400

    update SubmitBtn.tsx

--- src/features/modals/highlightModal/modalBody/SubmitBtn.tsx
+++ src/features/modals/highlightModal/modalBody/SubmitBtn.tsx
@@ -1,5 +1,11 @@
 const SubmitBtn = () => {
-    return <button className="border-t p-[15px]">Submit</button>;
+  return (
+    <div className="flex justify-end w-full text-center">
+      <button className="border-t py-[8px] rounded-md px-[12px] text-modalHeaderSize bg-primaryColor text-white ">
+        Submit
+      </button>
+    </div>
+  );
 };
 
 export default SubmitBtn;
