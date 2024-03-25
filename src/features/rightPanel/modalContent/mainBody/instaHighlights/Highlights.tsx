d805c6359672af388ef9647e21d0da2e3efe62d7
Sandro <vipsandro11@gmail.com>
2024-03-25 20:49:47 +0400

    feat: :sparkles: Integrate Redux state in Highlights component
    - Imported useAppSelector hook from reduxHooks for accessing Redux store state. - Added logic to check if highlights array is empty to conditionally render ShowHighlightsBtn component. - Modified the rendering logic to include ShowHighlightsBtn only when highlights array is empty. - Integrated Redux state to determine whether to display highlights or not based on the showHighlights state.

--- src/features/rightPanel/modalContent/mainBody/instaHighlights/Highlights.tsx
+++ src/features/rightPanel/modalContent/mainBody/instaHighlights/Highlights.tsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import ShowHighlightsBtn from "./ShowHighlightsBtn";
 import MainHighlights from "./mainHighlights/MainHighlights";
+import { useAppSelector } from "../../../../../hooks/reduxHooks";
 
 const Highlights = () => {
   const [showHighlights, setShowHighlights] = useState<boolean>(false);
+  const { highlights } = useAppSelector((store) => store.highlights);
+  const isHighlightsEmpty = highlights.length < 1;
   return (
     <div>
-      <ShowHighlightsBtn
-        setShowHighlights={setShowHighlights}
-        showHighlights={showHighlights}
-      />
+      {isHighlightsEmpty && (
+        <ShowHighlightsBtn
+          setShowHighlights={setShowHighlights}
+          showHighlights={showHighlights}
+        />
+      )}
       {showHighlights && <MainHighlights />}
     </div>
   );
