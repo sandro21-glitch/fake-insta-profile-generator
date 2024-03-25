3617bdeb5777e01aba6ab04166bba8e7e5934731
Sandro <vipsandro11@gmail.com>
2024-03-25 20:49:05 +0400

    feat: Enhance ModalHeader component with Redux integration

--- src/features/modals/highlightModal/modalHeader/ModalHeader.tsx
+++ src/features/modals/highlightModal/modalHeader/ModalHeader.tsx
@@ -1,9 +1,12 @@
 import { IoMdClose } from "react-icons/io";
+import { useAppDispatch } from "../../../../hooks/reduxHooks";
+import { setHighlightsModalClose } from "../../../slices/highlightsSlice";
 const ModalHeader = () => {
+  const dispatch = useAppDispatch();
   return (
     <header className="flex justify-between w-full min-h-[16.43px] p-[15px] border border-b-[#e5e5e5]">
-      <p>Add Highlight</p>
-      <button>
+      <p className="text-modalHeaderSize">Add Highlight</p>
+      <button type="button" onClick={() => dispatch(setHighlightsModalClose())}>
         <IoMdClose />
       </button>
     </header>
