618414803e7a90581fdd0d51d7cce7baf265fc20
Sandro <vipsandro11@gmail.com>
2024-03-25 20:50:32 +0400

    feat: Improve rendering logic in MainHighlights component

--- src/features/rightPanel/modalContent/mainBody/instaHighlights/mainHighlights/MainHighlights.tsx
+++ src/features/rightPanel/modalContent/mainBody/instaHighlights/mainHighlights/MainHighlights.tsx
@@ -1,10 +1,13 @@
+import { useAppSelector } from "../../../../../../hooks/reduxHooks";
 import HighlightsBar from "./HighlightsBar";
 import HighlightsList from "./HighlightsList";
 
 const MainHighlights = () => {
+  const { highlights } = useAppSelector((store) => store.highlights);
+  const isHighlightsEmpty = highlights.length < 1;
   return (
-    <div>
-      <HighlightsBar />
+    <div className="overflow-scroll">
+      {isHighlightsEmpty && <HighlightsBar />}
       <HighlightsList />
     </div>
   );
