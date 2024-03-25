761f72bf66951934f0745110a276dcec50c33237
Sandro <vipsandro11@gmail.com>
2024-03-25 20:48:51 +0400

    feat: Refactor UploadImage component to handle image upload and preview

--- src/features/modals/highlightModal/modalBody/UploadImage.tsx
+++ src/features/modals/highlightModal/modalBody/UploadImage.tsx
@@ -1,12 +1,43 @@
-const UploadImage = () => {
+import { ChangeEvent } from "react";
+
+interface UploadImageProps {
+  setHighlightImage: React.Dispatch<React.SetStateAction<string>>;
+  highlightImage: string;
+}
+const UploadImage = ({
+  setHighlightImage,
+  highlightImage,
+}: UploadImageProps) => {
+  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const imageDataUrl = reader.result as string;
+      setHighlightImage(imageDataUrl);
+    };
+
+    reader.readAsDataURL(file);
+  };
+
   return (
     <label
       htmlFor="image"
       className="block cursor-pointer w-full max-w-[300px] max-h-[300px] mb-[30px] border-2 border-dashed border-gray-500 rounded-lg"
     >
-      <input type="file" id="image" className="hidden" />
+      <input
+        type="file"
+        id="image"
+        className="hidden"
+        onChange={handleImageChange}
+      />
       <img
-        src="https://fakedetail.com/assets/images/addimgbg.jpg"
+        src={`${
+          highlightImage === ""
+            ? "https://fakedetail.com/assets/images/addimgbg.jpg"
+            : highlightImage
+        }`}
         alt="image"
         className="w-full h-auto"
       />
