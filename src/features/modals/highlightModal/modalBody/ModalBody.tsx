bcbf38472e00e54f06ff8ee01e7b391dd899229e
Sandro <vipsandro11@gmail.com>
2024-03-25 20:48:09 +0400

    update ModalBody.tsx

--- src/features/modals/highlightModal/modalBody/ModalBody.tsx
+++ src/features/modals/highlightModal/modalBody/ModalBody.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import HighlightNameInput from "./HighlightNameInput";
 import SubmitBtn from "./SubmitBtn";
 import UploadImage from "./UploadImage";
 
 const ModalBody = () => {
+  const [highlightName, setHighlightName] = useState("");
+  const [highlightImage, setHighlightImage] = useState("");
+
   return (
     <div className="p-[15px]">
-      <UploadImage />
-      <HighlightNameInput />
-      <SubmitBtn />
+      <UploadImage
+        setHighlightImage={setHighlightImage}
+        highlightImage={highlightImage}
+      />
+      <HighlightNameInput
+        setHighlightName={setHighlightName}
+        highlightName={highlightName}
+      />
+      <SubmitBtn
+        highlightName={highlightName}
+        highlightImage={highlightImage}
+      />
     </div>
   );
 };
