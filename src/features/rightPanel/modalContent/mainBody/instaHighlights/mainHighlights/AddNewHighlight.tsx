a648fc79e224e93fe252670b1e413985c6208c09
Sandro <vipsandro11@gmail.com>
2024-03-25 20:50:03 +0400

    feat: Add functionality to open highlights modal

--- src/features/rightPanel/modalContent/mainBody/instaHighlights/mainHighlights/AddNewHighlight.tsx
+++ src/features/rightPanel/modalContent/mainBody/instaHighlights/mainHighlights/AddNewHighlight.tsx
@@ -1,7 +1,14 @@
 import { HiOutlinePlus } from "react-icons/hi";
+import { useAppDispatch } from "../../../../../../hooks/reduxHooks";
+import { setHighlightsModalOpen } from "../../../../../slices/highlightsSlice";
 const AddNewHighlight = () => {
+  const dispatch = useAppDispatch();
+
   return (
-    <li className="min-w-[80px] w-[80px] flex flex-col gap-[3px] items-center">
+    <li
+      onClick={() => dispatch(setHighlightsModalOpen())}
+      className="min-w-[80px] w-[80px] flex flex-col gap-[3px] items-center"
+    >
       <div
         className="w-[63px] h-[63px] border border-[#000] rounded-full mt-[3px] cursor-pointer
        text-modalHeaderSize flex justify-center items-center"
