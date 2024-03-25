076151645b06f141372d4e083c7c6d29826ec59e
Sandro <vipsandro11@gmail.com>
2024-03-25 20:51:12 +0400

    feat: :sparkles: Enhance highlightsSlice with additional state and actions
    - Imported uuidv4 function from "uuid" for generating unique IDs for highlights. - Updated highlightsState interface to include highlightsModal boolean flag. - Initialized highlightsModal state in the initialState object. - Added setHighlightsModalOpen and setHighlightsModalClose reducers to handle opening and closing of the highlights modal. - Added addNewHighlight reducer to handle adding new highlights to the state with generated IDs. - Exported setHighlightsModalOpen, setHighlightsModalClose, and addNewHighlight actions for external use.

--- src/features/slices/highlightsSlice.ts
+++ src/features/slices/highlightsSlice.ts
@@ -1,26 +1,49 @@
 import { createSlice } from "@reduxjs/toolkit";
-// import type { PayloadAction } from "@reduxjs/toolkit";
-// import { v4 as uuidv4 } from 'uuid';
-interface highlightsTypes {
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { v4 as uuidv4 } from "uuid";
+interface highlightTypes {
   id: string;
   name: string;
   image: string;
 }
 
 export interface highlightsState {
-  highlights: highlightsTypes[];
+  highlights: highlightTypes[];
+  highlightsModal: boolean;
 }
 
 const initialState: highlightsState = {
   highlights: [],
+  highlightsModal: false,
 };
 
 export const highlightsSlice = createSlice({
   name: "counter",
   initialState,
-  reducers: {},
+  reducers: {
+    setHighlightsModalOpen: (state) => {
+      state.highlightsModal = true;
+    },
+    setHighlightsModalClose: (state) => {
+      state.highlightsModal = false;
+    },
+    addNewHighlight: (
+      state,
+      action: PayloadAction<{ name: string; image: string }>
+    ) => {
+      const NewHighlight = {
+        id: uuidv4(),
+        ...action.payload,
+      };
+      state.highlights.push(NewHighlight);
+    },
+  },
 });
 
-export const {} = highlightsSlice.actions;
+export const {
+  setHighlightsModalOpen,
+  setHighlightsModalClose,
+  addNewHighlight,
+} = highlightsSlice.actions;
 
 export default highlightsSlice.reducer;
