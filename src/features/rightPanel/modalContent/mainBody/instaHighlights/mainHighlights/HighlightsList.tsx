9b0c53fc0d9e0f77595484fe6639f4b1d949d056
Sandro <vipsandro11@gmail.com>
2024-03-25 20:50:17 +0400

    feat: Dynamically render highlights based on Redux state

--- src/features/rightPanel/modalContent/mainBody/instaHighlights/mainHighlights/HighlightsList.tsx
+++ src/features/rightPanel/modalContent/mainBody/instaHighlights/mainHighlights/HighlightsList.tsx
@@ -1,15 +1,23 @@
+import { useAppSelector } from "../../../../../../hooks/reduxHooks";
 import AddNewHighlight from "./AddNewHighlight";
+import MainListItem from "./MainListItem";
 import TempListItem from "./TempListItem";
 
 const tempList = [{ id: 1 }, { id: 2 }, { id: 3 }];
 
 const HighlightsList = () => {
+  const { highlights } = useAppSelector((store) => store.highlights);
+
   return (
-    <ul className="flex justify-between">
+    <ul className={`flex ${highlights.length > 0 ? "flex-row-reverse justify-end" : ""}`}>
       <AddNewHighlight />
-      {tempList.map((list) => {
-        return <TempListItem key={list.id} />;
-      })}
+      {highlights.length < 1
+        ? tempList.map((list) => {
+            return <TempListItem key={list.id} />;
+          })
+        : highlights.map((highlight) => (
+            <MainListItem key={highlight.id} highlight={highlight} />
+          ))}
     </ul>
   );
 };
