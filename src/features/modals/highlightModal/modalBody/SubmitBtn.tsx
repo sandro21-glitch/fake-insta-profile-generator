92037f8ba5c95eb41ddcbee85d8b7b2755c8d827
Sandro <vipsandro11@gmail.com>
2024-03-25 20:48:38 +0400

    feat: Refactor SubmitBtn component to use Redux dispatch

--- src/features/modals/highlightModal/modalBody/SubmitBtn.tsx
+++ src/features/modals/highlightModal/modalBody/SubmitBtn.tsx
@@ -1,7 +1,32 @@
-const SubmitBtn = () => {
+import { useAppDispatch } from "../../../../hooks/reduxHooks";
+import {
+  addNewHighlight,
+  setHighlightsModalClose,
+} from "../../../slices/highlightsSlice";
+
+type SubmitBtnTypes = {
+  highlightName: string;
+  highlightImage: string;
+};
+
+const SubmitBtn = ({ highlightName, highlightImage }: SubmitBtnTypes) => {
+  const dispatch = useAppDispatch();
+  const newItem = {
+    name: highlightName,
+    image: highlightImage,
+  };
+  const handleAddNewHighlight = () => {
+    if (!highlightName) return;
+    dispatch(addNewHighlight(newItem));
+    dispatch(setHighlightsModalClose());
+  };
+
   return (
     <div className="flex justify-end w-full text-center">
-      <button className="border-t py-[8px] rounded-md px-[12px] text-modalHeaderSize bg-primaryColor text-white ">
+      <button
+        onClick={handleAddNewHighlight}
+        className="border-t py-[8px] rounded-md px-[12px] text-modalHeaderSize bg-primaryColor text-white "
+      >
         Submit
       </button>
     </div>
