cfbca3397ae1a7a5a61dbf6119121a250d8bbf8d
Sandro <vipsandro11@gmail.com>
2024-03-26 20:08:53 +0400

    update HighlightModal.tsx

--- src/features/modals/highlightModal/HighlightModal.tsx
+++ src/features/modals/highlightModal/HighlightModal.tsx
@@ -1,10 +1,17 @@
+import { useEffect } from "react";
 import ModalContent from "./ModalContent"
 
 
 const HighlightModal = () => {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, []);
   return (
-    <div className="absolute inset-0 h-full w-full z-[9999] flex justify-center items-center ">
-      <div className="absolute inset-0 bg-black opacity-65 z-[-1]"></div>
+    <div className="fixed inset-0 h-screen w-full z-[9999] flex justify-center items-center ">
+      <div className="fixed inset-0 bg-black opacity-65 z-[-1]"></div>
         <ModalContent />
     </div>
   )
