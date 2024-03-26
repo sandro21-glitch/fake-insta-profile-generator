c090debf90e2e46ba5d780a86e0e8f1418abd060
Sandro <vipsandro11@gmail.com>
2024-03-26 15:46:51 +0400

    efactor: Convert MainListItem to use input field for editing names

--- src/features/rightPanel/modalContent/mainBody/instaHighlights/mainHighlights/MainListItem.tsx
+++ src/features/rightPanel/modalContent/mainBody/instaHighlights/mainHighlights/MainListItem.tsx
@@ -1,11 +1,39 @@
+import { useRef, useState } from "react";
+import useOutsideClickAndFocus from "../../../../../../hooks/useOutsideClickAndFocus";
+import InputField from "../../../../../../ui/InputField";
+import { useAppDispatch } from "../../../../../../hooks/reduxHooks";
+import { editHighlightName } from "../../../../../slices/highlightsSlice";
+
 type MainListItemTypes = {
   highlight: { id: string; name: string; image: string };
 };
 
 const MainListItem = ({ highlight }: MainListItemTypes) => {
-  const { image, name } = highlight;
+  const { image, name, id } = highlight;
+  const [editName, setEditName] = useState("");
+  const [isInputClicked, setIsInputClicked] = useState(false);
+  const dispatch = useAppDispatch();
+  const containerRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useOutsideClickAndFocus({
+    containerRef,
+    inputRef,
+    isInputClicked,
+    setIsInputClicked,
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newName = e.target.value;
+    setEditName(newName);
+    dispatch(editHighlightName({ id, editName: newName }));
+  };
+
   return (
-    <li className="min-w-[80px] w-[80px] flex flex-col gap-[3px] items-center">
+    <div
+      ref={containerRef}
+      className="min-w-[80px] w-[80px] flex flex-col gap-[3px] items-center"
+    >
       <div
         className="w-[63px] h-[63px] rounded-full mt-[3px] cursor-pointer
      text-modalHeaderSize flex justify-center items-center overflow-hidden relative
@@ -13,10 +41,28 @@ const MainListItem = ({ highlight }: MainListItemTypes) => {
     before:border before:border-[#dedede] before:rounded-full before:w-[62px] before:h-[62px]
      "
       >
-          <img src={image} alt={name} className="w-[56px] h-[56px] rounded-full" />
+        <img
+          src={image}
+          alt={name}
+          className="w-[56px] h-[56px] rounded-full"
+        />
       </div>
-      <p className="text-[12px]">{name}</p>
-    </li>
+      {isInputClicked ? (
+        <InputField
+          className="w-full text-center text-[12px]"
+          value={editName}
+          onChange={handleInputChange}
+          inputRef={inputRef}
+        />
+      ) : (
+        <p
+          onClick={() => setIsInputClicked(true)}
+          className="text-[12px] text-startw-full"
+        >
+          {name}
+        </p>
+      )}
+    </div>
   );
 };
 
