b9404d791cd8b4cc785b94800a6a528f3574b613
Sandro <vipsandro11@gmail.com>
2024-03-26 19:09:40 +0400

    update TabContent.tsx

--- src/features/rightPanel/modalContent/mainBody/tabContent/TabContent.tsx
+++ src/features/rightPanel/modalContent/mainBody/tabContent/TabContent.tsx
@@ -1,9 +1,11 @@
 import AddPostsBtn from "./AddPostsBtn";
+import UserPosts from "./posts/UserPosts";
 
 const TabContent = () => {
   return (
     <section className="h-[440px]">
       <AddPostsBtn />
+      <UserPosts />
     </section>
   );
 };
