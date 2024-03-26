626de79696270ea4f02b976b1e8ea027b7d0d16e
Sandro <vipsandro11@gmail.com>
2024-03-26 20:09:00 +0400

    feat: create PostsModalContent.tsx

---
+++ src/features/modals/postsModal/PostsModalContent.tsx
@@ -0,0 +1,9 @@
+const PostsModalContent = () => {
+  return (
+    <div className="w-[300px] max-w-[300px] bg-white rounded-md">
+      PostsModalContent
+    </div>
+  );
+};
+
+export default PostsModalContent;
