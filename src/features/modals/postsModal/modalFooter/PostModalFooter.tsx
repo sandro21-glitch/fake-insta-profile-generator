1469a37295e97cdb6fa26f10a6be8695b4b48c75
Sandro <vipsandro11@gmail.com>
2024-03-27 22:14:10 +0400

    update PostModalFooter.tsx

--- src/features/modals/postsModal/modalFooter/PostModalFooter.tsx
+++ src/features/modals/postsModal/modalFooter/PostModalFooter.tsx
@@ -1,8 +1,24 @@
-const PostModalFooter = () => {
+import { useAppDispatch } from "../../../../hooks/reduxHooks";
+import { addNewPost, closePostsModal } from "../../../slices/postsSlice";
+
+type ModalFooterTypes = {
+  postImage: string;
+  postType: string;
+};
+
+const PostModalFooter = ({ postImage, postType }: ModalFooterTypes) => {
+  const dispatch = useAppDispatch();
+
+  const handleAddPost = () => {
+    dispatch(addNewPost({ postImage, postType }));
+    dispatch(closePostsModal());
+  };
+
   return (
     <div className="border border-t-[#e5e5e5] p-[15px] flex justify-end">
       <button
         type="button"
+        onClick={handleAddPost}
         className="h-[38px] w-auto bg-primaryColor text-white py-[6px] px-[12px] rounded-md text-modalHeaderSize"
       >
         Post
