01038e44d284870d4f29bc28975876a9788bd1d5
Sandro <vipsandro11@gmail.com>
2024-03-27 22:14:05 +0400

    update PostModalBody.tsx

--- src/features/modals/postsModal/modalBody/PostModalBody.tsx
+++ src/features/modals/postsModal/modalBody/PostModalBody.tsx
@@ -4,12 +4,17 @@ import UploadPostImage from "./UploadPostImage";
 type PostBodyTypes = {
   postImage: string;
   setPostImage: (img: string) => void;
+  setPostType: (type: string) => void;
 };
-const PostModalBody = ({ postImage,setPostImage }: PostBodyTypes) => {
+const PostModalBody = ({
+  postImage,
+  setPostImage,
+  setPostType,
+}: PostBodyTypes) => {
   return (
     <div className="p-[15px]">
       <UploadPostImage postImage={postImage} setPostImage={setPostImage} />
-      <PostTypeList />
+      <PostTypeList setPostType={setPostType} />
     </div>
   );
 };
