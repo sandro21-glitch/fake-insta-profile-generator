44467394e0a257ccac6aa9b608b12f1505f363fd
Sandro <vipsandro11@gmail.com>
2024-03-27 21:09:44 +0400

    update PostsModalContent.tsx

--- src/features/modals/postsModal/PostsModalContent.tsx
+++ src/features/modals/postsModal/PostsModalContent.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import PostModalBody from "./modalBody/PostModalBody";
+import PostModalHeader from "./modalHeader/PostModalHeader";
+import PostModalFooter from "./modalFooter/PostModalFooter";
+
 const PostsModalContent = () => {
+  const [postImage, setPostImage] = useState<string>("");
+
   return (
-    <div className="w-[300px] max-w-[300px] bg-white rounded-md">
-      PostsModalContent
+    <div className="w-[300px] max-w-[300px] bg-white rounded-md overflow-hidden">
+      <PostModalHeader />
+      <PostModalBody postImage={postImage} setPostImage={setPostImage} />
+      <PostModalFooter />
     </div>
   );
 };
