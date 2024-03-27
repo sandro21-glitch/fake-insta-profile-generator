f9c9e5f7f5f113f5f91ec132707352e4e237d104
Sandro <vipsandro11@gmail.com>
2024-03-27 21:09:54 +0400

    feat: create PostModalFooter.tsx

---
+++ src/features/modals/postsModal/modalFooter/PostModalFooter.tsx
@@ -0,0 +1,14 @@
+const PostModalFooter = () => {
+  return (
+    <div className="border border-t-[#e5e5e5] p-[15px] flex justify-end">
+      <button
+        type="button"
+        className="h-[38px] w-auto bg-primaryColor text-white py-[6px] px-[12px] rounded-md text-modalHeaderSize"
+      >
+        Post
+      </button>
+    </div>
+  );
+};
+
+export default PostModalFooter;
