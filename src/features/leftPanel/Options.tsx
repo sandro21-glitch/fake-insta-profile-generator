a03e2480b4f15138d7b1074fbc782c1d2a0a04ba
Sandro <vipsandro11@gmail.com>
2024-03-28 22:49:03 +0400

    update Options.tsx

--- src/features/leftPanel/Options.tsx
+++ src/features/leftPanel/Options.tsx
@@ -3,24 +3,25 @@ import HiddenOptions from "./options/HiddenOptions";
 import TopOptions from "./options/TopOptions";
 
 const Options = () => {
-  const [hiddenOptions, setHiddenOptions] = useState(true);
+  const [hiddenOptions, setHiddenOptions] = useState(false);
 
   return (
     <section>
       <TopOptions />
-      {hiddenOptions ? (
-        <HiddenOptions />
-      ) : (
-        <div>
-          <label htmlFor="show">More Options</label>
-          <input
-            onChange={() => setHiddenOptions(!hiddenOptions)}
-            type="checkbox"
-            name="show"
-            id="show"
-          />
-        </div>
-      )}
+      {hiddenOptions && <HiddenOptions />}
+      <div className="flex items-center md:hidden cursor-pointer">
+        <input
+          onChange={() => setHiddenOptions(!hiddenOptions)}
+          type="checkbox"
+          name="show"
+          id="show"
+          className="cursor-pointer"
+          checked={hiddenOptions}
+        />
+        <label htmlFor="show" className="ml-2 cursor-pointer">
+          More Options
+        </label>
+      </div>
     </section>
   );
 };
