defd4892a7eeb81213b655d4226380bd3b69be43
Sandro <vipsandro11@gmail.com>
2024-03-28 22:32:39 +0400

    update Options.tsx

--- src/features/leftPanel/Options.tsx
+++ src/features/leftPanel/Options.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import HiddenOptions from "./options/HiddenOptions";
 import TopOptions from "./options/TopOptions";
 
 const Options = () => {
+  const [hiddenOptions, setHiddenOptions] = useState(true);
+
   return (
     <section>
       <TopOptions />
-      <HiddenOptions />
+      {hiddenOptions && <HiddenOptions />}
     </section>
   );
 };
