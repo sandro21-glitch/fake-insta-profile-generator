326a8c3cffc1d568f53a3235c130d34bee714540
Sandro <vipsandro11@gmail.com>
2024-03-28 20:44:00 +0400

    update UserPosts.tsx

--- src/features/rightPanel/modalContent/mainBody/tabContent/posts/UserPosts.tsx
+++ src/features/rightPanel/modalContent/mainBody/tabContent/posts/UserPosts.tsx
@@ -1,11 +1,12 @@
 import { useAppSelector } from "../../../../../../hooks/reduxHooks";
+import NoPosts from "./NoPosts";
 import SinglePost from "./SinglePost";
 
 const UserPosts = () => {
   const { posts } = useAppSelector((store) => store.posts);
-  if (posts.length < 1) return <div>No Posts</div>;
+  if (posts.length < 1) return <NoPosts />;
   return (
-    <ul className="flex flex-wrap">
+    <ul className="flex flex-wrap h-full">
       {posts.map((post) => {
         return <SinglePost key={post.id} post={post} />;
       })}
