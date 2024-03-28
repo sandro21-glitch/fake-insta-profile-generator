1c646518a37054b87600bb09d95c25c3b8181688
Sandro <vipsandro11@gmail.com>
2024-03-28 20:43:58 +0400

    update AddPostsBtn.tsx

--- src/features/rightPanel/modalContent/mainBody/tabContent/AddPostsBtn.tsx
+++ src/features/rightPanel/modalContent/mainBody/tabContent/AddPostsBtn.tsx
@@ -1,11 +1,18 @@
 import { FaCloudUploadAlt } from "react-icons/fa";
+import { useAppDispatch } from "../../../../../hooks/reduxHooks";
+import { openPostsModal } from "../../../../slices/postsSlice";
 
 const AddPostsBtn = () => {
+  const dispatch = useAppDispatch();
+
   return (
-    <button className="text-headerSize w-full h-auto bg-primaryColor mt-[1px]
-     text-white flex justify-center py-2 rounded-[3px]">
+    <button
+      onClick={() => dispatch(openPostsModal())}
+      className="text-headerSize w-full h-auto bg-primaryColor mt-[1px]
+     text-white flex items-center justify-center py-2 rounded-[3px]"
+    >
       <span className="capitalize">add post</span>
-      <FaCloudUploadAlt />
+      <FaCloudUploadAlt className="ml-1" />
     </button>
   );
 };
